5cc7b0cb4e4a4223ace1741eef6c0ae6e7101d80
LZhang98 <luke.taotao@hotmail.com>
2020-11-07 17:23:21 -0500

    testing blog for VCF files

---
+++ glob_test.py
@@ -0,0 +1,20 @@
+import json
+import vcf
+import glob
+
+# Helper function to extract individual name from filepath
+def remove_prefix(my_string, prefix, suffix):
+    my_string = my_string.lstrip(prefix)
+    my_string = my_string.rstrip(suffix)
+    return my_string
+
+file_list = glob.glob("../PoplarVCFsAnnotated/*.filter.vcf")
+
+data = {}
+
+destf = open('main_db.json', 'w')
+
+for f in file_list:
+    individual = remove_prefix(f, '../PoplarVCFsAnnotated/', '.filter.vcf')
+    curr_individual = {}
+    print("reading "+individual)
