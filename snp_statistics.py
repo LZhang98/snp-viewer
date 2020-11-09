7978d27c869deeff3368ebbad06d5395e22394e9
LZhang98 <luke.taotao@hotmail.com>
2020-11-08 20:09:58 -0500

    snp statistics for BAR

--- snp_statistics.py
+++ snp_statistics.py
@@ -22,28 +22,51 @@ writer = csv.writer(destf, delimiter=',')
 print('loading database')
 data = json.load(db)
 
-writer.writerow(['transcript_id', 'gene_length','num_snps','num_snps_unique', 'mean_snp_spacing'])
+writer.writerow(['transcript_id',
+                 'gene_length',
+                 'num_snps',
+                 'snps_per_kb',
+                 'num_snps_unique',
+                 'unique_snps_per_kb',
+                 'mean_snp_spacing'])
 
 num_entries = 0
+
+#build up row by row and add
 for transcript in data.keys():
+
+    # transcript_id
     entry = [transcript]
-    snps = data[transcript]
 
-    # TODO: gene length
-    # perhaps read separately
+    # gene length
+    gene_length = data[transcript]["len"]
+    entry.append(gene_length)
 
-    # non-unique SNP occurences
-    entry.append(len(snps))
+    snps = data[transcript]["pos"]
 
-    # unique SNP occurences
+    # non-unique SNP occurences (num_snps)
+    num_snps = len(snps)
+    entry.append(num_snps)
+
+    # snps_per_kb
+    length_kb = float(gene_length)/1000
+    snps_per_kb = num_snps/length_kb
+    entry.append(round(snps_per_kb, 5))
+
+    # unique SNP occurences (num_snps_unique)
     unique_snps = list(set(snps))
-    entry.append(len(unique_snps))
+    num_unique = len(unique_snps)
+    entry.append(num_unique)
+
+    # unique snps per kb
+    unique_snps_per_kb = num_unique/length_kb
+    entry.append(round(unique_snps_per_kb, 5))
 
     # mean snp spacing
-    arr = np.asarray(unique_snps)
+    arr = np.asarray(sorted(unique_snps))
     spacing = np.diff(arr)
     mean_spacing = np.mean(spacing)
-    entry.append(mean_spacing)
+    entry.append(round(mean_spacing, 5))
 
     writer.writerow(entry)
 
